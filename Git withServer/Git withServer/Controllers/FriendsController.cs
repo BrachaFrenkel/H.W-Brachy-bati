b6cdf831c74282ad2247cd9cddeafcb97b20c2fc
פרנקל ברכה < @mbjcomp.local>
2023-12-05 19:52:53 +0200

    add ontroller

---
+++ Git withServer/Git withServer/Controllers/FriendsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Git_withServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FriendsController : ControllerBase
+    {
+    }
+}
